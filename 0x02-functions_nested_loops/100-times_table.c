2b22c648a5c0acdbae0e63a06f00441a712cc1bf
KIPACHA <godffreyngotho@gmail.com>
2023-02-21 12:11:25 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,47 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - print a sequence from a given number to 98
- * @n: strating number
+ * print_times_table - print a times table for a given number n
+ * @n: the maximum number
  *
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	while (n != 98)
+	int i, j, product;
+
+	if (n >= 0 && n <= 15)
 	{
-		printf("%d", n);
-		if (n > 98)
-			--n;
-		else if (n < 98)
-			++n;
-		printf(", ");
+		for (i = 0; i <= n; ++i)
+		{
+			for (j = 0; j <= n; ++j)
+			{
+				product = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product < 100)
+						_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
+				}
+				if (product >= 100)
+				{
+					_putchar('0' + product / 100);
+					_putchar('0' + (product % 100) / 10);
+					_putchar('0' + (product % 10));
+				}
+				else if (product <= 9)
+				{
+					_putchar('0' + product);
+				}
+				else
+				{
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+			}
+			_putchar('\n');
+		}
 	}
-	printf("%d", 98);
-	printf("\n");
 }
