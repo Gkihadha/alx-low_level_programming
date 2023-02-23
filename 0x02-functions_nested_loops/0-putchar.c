7b169d20dc145c5b6964696937ee4223c78a3c01
KIPACHA <godffreyngotho@gmail.com>
2023-02-23 11:59:10 -0800

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include "_main.h"
-=======
-#include <unistd.h>
->>>>>>> ca5104559f6de5f7586bb7b20710c4fb1917edf0
-=======
 #include "main.h"
->>>>>>> 3645eb4ae67c701d288c8e96e510a6b0240ad13d
 
 /**
  * main - Entry point
