1f15f2acf376523570d8772536b62fe6b2170586
Gkihadha <godffreyngotho@gmail.com>
2023-03-14 02:46:11 -0700

    updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,42 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * argstostr - concatenates all the arguments of a program.
- * @ac: argument count.
- * @av: argument vector.
- *
- * Return: pointer of an array of char
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	char *aout;
-	int c, i, j, ia;
+	int i, n, k = 0, len = 0;
+	char *str;
 
-	if (ac == 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (c = i = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		if (av[i] == NULL)
-			return (NULL);
-
-		for (j = 0; av[i][j] != '\0'; j++)
-			c++;
-		c++;
+		for (n = 0; av[i][n]; n++)
+			len++;
 	}
+	len += ac;
 
-	aout = malloc((c + 1) * sizeof(char));
-
-	if (aout == NULL)
-	{
-		free(aout);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
 		return (NULL);
-	}
 
-	for (i = j = ia = 0; ia < c; j++, ia++)
+	for (i = 0; i < ac; i++)
 	{
-		if (av[i][j] == '\0')
+		for (n = 0; av[i][n]; n++)
 		{
-			aout[ia] = '\n';
-			i++;
-			ia++;
-			j = 0;
+			str[k] = av[i][n];
+			k++;
 		}
-		if (ia < c - 1)
-			aout[ia] = av[i][j];
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
 	}
-	aout[ia] = '\0';
-
-	return (aout);
+	return (str);
 }
